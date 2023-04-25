ca811c53ff0a47db899a921062a6e5cc99a61b68
Krish Pandya <kpandya@umd.edu>
2023-04-25 18:02:00 -0400

    Initial Commit

---
+++ GroupApp436/GroupApp436App.swift
@@ -0,0 +1,17 @@
+//
+//  GroupApp436App.swift
+//  GroupApp436
+//
+//  Created by Krish Pandya on 4/25/23.
+//
+
+import SwiftUI
+
+@main
+struct GroupApp436App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
